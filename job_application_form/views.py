ae12d51418ad55d5fc7dfbde10f0fae213b14223
gaoissac2019 <48165303+gaoissac2019@users.noreply.github.com>
2023-06-27 22:06:58 +0200

    add nav bar and extra pages

--- job_application_form/views.py
+++ job_application_form/views.py
@@ -28,3 +28,11 @@ def index(request):
                                          to=[email])
             email_message.send()
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, 'contact.html')
